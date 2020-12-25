59b3489d3f8f26cb080017a7e87463e600ae8cf3
FikryAS <azizfikry15@gmail.com>
2020-12-25 13:09:08 +0700

    update main.dart and create page login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildListDelegate([]),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Container(
+              color: Colors.red,
+              child: Text("Sliver Fill Remaining"),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/login_page.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
